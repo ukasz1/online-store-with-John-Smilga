82f0c9e3240d58ff9f4c7300f167480ca9b355c0
ukasz1 <lukaszmitkowski53@gmail.com>
2022-02-11 15:23:16 +0100

    category and company filter added

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -20,6 +20,13 @@ const Filters = () => {
     clearFilters,
     all_products
   } = useFilterContext();
+
+  const categories = getUniqueValues(all_products, 'category');
+  const companies = getUniqueValues(all_products, 'company');
+  const colors = getUniqueValues(all_products, 'colors');
+
+
+
   return <Wrapper>
     <div className='content'>
       <form onSubmit={e => e.preventDefault()}>
@@ -35,6 +42,37 @@ const Filters = () => {
           />
         </div>
         {/* end search input */}
+        {/* categories */}
+        <div className='form-control'>
+          <h5>category</h5>
+          <div>{
+            categories.map((c, index) => {
+              return <button
+                key={index}
+                onClick={updateFilters}
+                type="button"
+                name="category"
+                className={`${category === c.toLowerCase() ? 'active' : null
+                  }`}
+              >
+                {c}
+              </button>
+            })
+          }</div>
+        </div>
+        {/* end of categories */}
+        {/* companies */}
+        <div className='form-control'>
+          <h5>company</h5>
+          <select name="company" className='company' value={company} onChange={updateFilters}>
+            {companies.map((c, index) => {
+              return <option key={index} value={c}>
+                {c}
+              </option>
+            })}
+          </select>
+        </div>
+        {/* end of companies */}
       </form>
     </div>
   </Wrapper>

e5f00d622e5b076c44f7a2ddfc82501968b4aaf6
ukasz1 <lukaszmitkowski53@gmail.com>
2022-02-11 13:09:15 +0100

    first filter

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -40,8 +40,9 @@ export const FilterProvider = ({ children }) => {
   }, [products]);
 
   useEffect(() => {
-    dispatch({ type: SORT_PRODUCTS })
-  }, [products, state.sort]);
+    dispatch({ type: FILTER_PRODUCTS });
+    dispatch({ type: SORT_PRODUCTS });
+  }, [products, state.sort, state.filters]);
 
   const setGridView = () => {
     dispatch({ type: SET_GRIDVIEW })
@@ -54,14 +55,23 @@ export const FilterProvider = ({ children }) => {
     const value = e.target.value;
     dispatch({ type: UPDATE_SORT, payload: value })
   }
+  const updateFilters = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    dispatch({ type: UPDATE_FILTERS, payload: { name, value } })
+  }
+  const clearFilters = () => {
 
+  }
 
   return (
     <FilterContext.Provider value={{
       ...state,
       setGridView,
       setListView,
-      updateSort
+      updateSort,
+      updateFilters,
+      clearFilters
     }}>
       {children}
     </FilterContext.Provider>

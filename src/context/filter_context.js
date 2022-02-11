4e46096c60b4241272b26596aeb17841d9eae6db
ukasz1 <lukaszmitkowski53@gmail.com>
2022-02-11 12:12:23 +0100

    list view and grid view for products

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -15,7 +15,18 @@ import { useProductsContext } from './products_context'
 const initialState = {
   filtered_products: [],
   all_products: [],
-  grid_view: false
+  grid_view: true,
+  sort: 'price-lowest',
+  filters: {
+    text: '',
+    company: 'all',
+    category: 'all',
+    color: 'all',
+    min_price: 0,
+    max_price: 0,
+    price: 0,
+    shipping: false
+  }
 }
 
 const FilterContext = React.createContext()
@@ -28,8 +39,30 @@ export const FilterProvider = ({ children }) => {
     dispatch({ type: LOAD_PRODUCTS, payload: products })
   }, [products]);
 
+  useEffect(() => {
+    dispatch({ type: SORT_PRODUCTS })
+  }, [products, state.sort]);
+
+  const setGridView = () => {
+    dispatch({ type: SET_GRIDVIEW })
+  }
+  const setListView = () => {
+    dispatch({ type: SET_LISTVIEW })
+  }
+
+  const updateSort = (e) => {
+    const value = e.target.value;
+    dispatch({ type: UPDATE_SORT, payload: value })
+  }
+
+
   return (
-    <FilterContext.Provider value={{ ...state }}>
+    <FilterContext.Provider value={{
+      ...state,
+      setGridView,
+      setListView,
+      updateSort
+    }}>
       {children}
     </FilterContext.Provider>
   )
--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -11,13 +11,47 @@ import {
 
 const filter_reducer = (state, action) => {
   if (action.type === LOAD_PRODUCTS) {
+    let maxPrice = action.payload.map(p => p.price) //array of prices first
+    maxPrice = Math.max(...maxPrice);
+
     return {
       ...state,
-      all_products: [...action.payload],    // !!!!! need to copy in this way
-      filtered_products: [...action.payload]
+      all_products: [...action.payload],    // copying the array
+      filtered_products: [...action.payload],
+      filters: { ...state.filters, max_price: maxPrice, price: maxPrice }
     }
   }
-  return state
+  if (action.type === SET_GRIDVIEW) {
+    return { ...state, grid_view: true }
+  }
+  if (action.type === SET_LISTVIEW) {
+    return { ...state, grid_view: false }
+  }
+  if (action.type === UPDATE_SORT) {
+    return { ...state, sort: action.payload }
+  }
+  if (action.type === SORT_PRODUCTS) {
+    const { sort, filtered_products } = state;
+    let tempProducts = [...filtered_products];
+    if (sort === 'price-lowest') {
+      tempProducts = tempProducts.sort((a, b) => a.price - b.price);
+    }
+    if (sort === 'price-highest') {
+      tempProducts = tempProducts.sort((a, b) => b.price - a.price);
+    }
+    if (sort === 'name-a') {
+      tempProducts = tempProducts.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+      });
+    }
+    if (sort === 'name-z') {
+      tempProducts = tempProducts.sort((a, b) => {
+        return b.name.localeCompare(a.name);
+      });
+    }
+    return { ...state, filtered_products: tempProducts }
+  }
+
   throw new Error(`No Matching "${action.type}" - action type`)
 }
 

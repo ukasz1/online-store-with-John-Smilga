0529d14835dfe019f94a32418f0da5a4fec03b26
ukasz1 <lukaszmitkowski53@gmail.com>
2022-02-16 13:13:11 +0100

    complete project

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -63,10 +63,49 @@ const CheckoutForm = () => {
     // eslint-disable-next-line
   }, []);
 
-  const handleChange = async (event) => { }
-  const handleSubmit = async (ev) => { }
+  const handleChange = async (event) => {
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : '')
+  }
+  const handleSubmit = async (ev) => {
+    ev.preventDefault();
+    setProcessing(true);
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement)
+      }
+    })
+    if (payload.error) {
+      setError(`Payment failed ${payload.error.message}`);
+      setProcessing(false);
+    }
+    else {
+      setError(null);
+      setProcessing(false);
+      setSucceeded(true);
+      setTimeout(() => {
+        clearCart();
+        history.push('/');
+      }, 10000)
+    }
+  }
 
   return <div>
+    {
+      succeeded ? (
+        <article>
+          <h4>Thank you</h4>
+          <h4>Your payment was successful!</h4>
+          <h4>Redirecting to home page shortly</h4>
+        </article>
+      ) : (
+        <article>
+          <h4>Hello, {myUser && (myUser.name || myUser.nickname)}</h4>
+          <p>Your total is {formatPrice(shipping_fee + total_amount)} </p>
+          <p>Test Card Number : 4242 4242 4242 4242</p>
+        </article>
+      )
+    }
     <form id="payment-form" onSubmit={handleSubmit}>
       <CardElement
         id="card-element"
@@ -86,9 +125,7 @@ const CheckoutForm = () => {
       )}
       {/* Show success meddage upon completion */}
       <p className={succeeded ? 'result-message' : 'result-message hidden'}>
-        Payment succedded, see the result in your
-        <a href={`https://dashboard.stripe.com/test/payments`}>
-          Stripe dashboard
+        Payment succedded, see the result in your <a href={`https://dashboard.stripe.com/test/payments`}> Stripe dashboard
         </a>
       </p>
     </form>
--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -50,8 +50,6 @@ const products_reducer = (state, action) => {
     }
   }
 
-
-  return state
   throw new Error(`No Matching "${action.type}" - action type`)
 }
 
